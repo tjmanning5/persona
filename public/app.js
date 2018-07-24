6d2b1ef1041754874cdbad9cdd1f85884aab7baf
Travis Manning <tmannin2@gmail.com>
2018-07-24 17:08:36 -0400

    fleshed out html side and started on frontend JS

---
+++ public/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('#searchBar').submit(function (event) {
+
+        $('#infoField').empty();
+
+        event.preventDefault();
+
+        var personaSearch = $('#searchInput').val();
+
+
+    });
+});
