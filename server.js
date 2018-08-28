e63969408f21ffef8a820892c82f65113eb77529
Marina <mjbain10@gmail.com>
2018-08-27 20:33:49 -0400

    not the html controller tho

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.use(express.static('public'));
 
 // we were missing the below 4 lines, that imported the controller files and attached them to Express. So it didn't know that you had set a POST method (or any method for that matter) for the request URLs. My bad dude :(
 require("./routes/fusion-api-routes.js")(app);
-require("./routes/html-routes.js")(app);
 require("./routes/persona-api-routes.js")(app);
 require("./routes/skill-api-routes.js")(app);
+// require("./routes/html-routes.js")(app); // This file isn't set up yet, so I'm commenting out the import
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/persona', {useNewUrlParser: true});
